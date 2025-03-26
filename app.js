58b641d261a25260cea6c08efd83a7fab681de04
arshbhushan <arshbhushan@gmail.com>
2025-03-26 16:09:18 +0530

    updates syntax

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const express = require('express');
+import express from "express";
+//import { get404 } from "./controllers/error.js"; 
+
 const app = express();
-const router = express.Router();
-const errorController=require('./controllers/error')
 const port = 3000; // Choose a port number
 
-app.get('/', (req, res) => {
-  res.send('Hello, worldd!'); // Send a response to the root route
+app.get("/", (req, res) => {
+  res.send("Hello, world!"); // Send a response to the root route
 });
-app.use (errorController.get404);
+
+//app.use(get404);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
