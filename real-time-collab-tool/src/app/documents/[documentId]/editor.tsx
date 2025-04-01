da9dbf30d3b797b2321698e49047bf272c25f0f8
arshbhushan <arshbhushan@gmail.com>
2025-04-01 13:24:53 +0530

    added editor.

---
+++ real-time-collab-tool/src/app/documents/[documentId]/editor.tsx
@@ -0,0 +1,22 @@
+"use client"; //as server components are static[won't allow the usage of hooks] it will not let us use the editor thats why we are using "use client".
+import { useEditor, EditorContent } from '@tiptap/react';
+import StarterKit from "@tiptap/starter-kit";
+
+export const Editor = () => {
+    const editor = useEditor({
+        editorProps:{
+            attributes: {
+              class: "focus-outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pt-14 pb-10 cursor-text"
+            },
+        },
+        extensions: [StarterKit],
+        content: '<p>Hello World! 🌎️</p>',
+    })
+    return (
+        <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+            <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
+            <EditorContent editor={editor}/>
+            </div>
+        </div>
+    );
+}; 
