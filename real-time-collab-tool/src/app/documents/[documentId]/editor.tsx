7b9524565409a38ad9bdbc4a92f9a17e2da96b68
arshbhushan <arshbhushan@gmail.com>
2025-04-03 15:07:18 +0530

    implemented tables via hardcoding for tesitng.

--- real-time-collab-tool/src/app/documents/[documentId]/editor.tsx
+++ real-time-collab-tool/src/app/documents/[documentId]/editor.tsx
@@ -3,6 +3,10 @@ import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from "@tiptap/starter-kit";
 import TaskItem from '@tiptap/extension-task-item';
 import TaskList from '@tiptap/extension-task-list';
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
 
 export const Editor = () => {
     const editor = useEditor({
@@ -14,12 +18,31 @@ export const Editor = () => {
         },
         extensions: [
             StarterKit,
+            Table,
+            TableCell,
+            TableHeader,
+            TableRow,
             TaskItem.configure({
                 nested: true,
             }),
             TaskList,
         ],
-        content: '<p>Hello World! 🌎️</p>',
+        content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
     })
     return (
         <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
