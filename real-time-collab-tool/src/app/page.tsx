97a93d0222f0e2b58eaa9c1ef79d2d9f8be3c5f3
arshbhushan <arshbhushan@gmail.com>
2025-04-06 23:27:46 +0530

    fixing UI.

--- real-time-collab-tool/src/app/page.tsx
+++ real-time-collab-tool/src/app/page.tsx
@@ -3,7 +3,7 @@
 const Home = () => {
   return(
     <div className="flex min-h-screen items-center justify-center">
-      Click<Link href="/documents/123"><span className="text-blue-500 underline">&nbsp;here&nbsp;</span></Link> to go to document ID.
+      Click<Link href="/documents/123"><span className="text-blue-500 underline">&nbsp;here&nbsp;</span></Link> to go to document IDs.
     </div>
   )
 }
