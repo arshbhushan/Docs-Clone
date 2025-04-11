af23721b9621c57ff17af1a191b65b72e102dd53
arshbhushan <arshbhushan@gmail.com>
2025-04-11 23:08:23 +0530

    added mapping.

--- real-time-collab-tool/src/app/documents/[documentId]/toolbar.tsx
+++ real-time-collab-tool/src/app/documents/[documentId]/toolbar.tsx
@@ -79,6 +79,9 @@ export const Toolbar=()=>{
             <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
             {/*TODO: Font size */}
             <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
+            {sections[1].map((item)=>(
+                <ToolbarButton key={item.label} {...item} />
+            ))}
         </div>
     )
 }
