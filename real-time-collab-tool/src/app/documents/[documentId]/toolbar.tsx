976a018918c9c58c89036251715f9f9709316f08
arshbhushan <arshbhushan@gmail.com>
2025-04-23 14:37:37 +0530

    implemented HighlightColorButton component.

--- real-time-collab-tool/src/app/documents/[documentId]/toolbar.tsx
+++ real-time-collab-tool/src/app/documents/[documentId]/toolbar.tsx
@@ -2,7 +2,7 @@
 
 import { type ColorResult, SketchPicker } from "react-color";
 
-import { LucideIcon, Undo2Icon, Redo2Icon, PrinterIcon, SpellCheckIcon, BoldIcon, ItalicIcon, UnderlineIcon, MessageSquareCodeIcon, MessageSquarePlusIcon, ListTodoIcon, RemoveFormattingIcon, ChevronDownIcon } from "lucide-react";
+import { LucideIcon, Undo2Icon, Redo2Icon, PrinterIcon, SpellCheckIcon, BoldIcon, ItalicIcon, UnderlineIcon, MessageSquareCodeIcon, MessageSquarePlusIcon, ListTodoIcon, RemoveFormattingIcon, ChevronDownIcon, HighlighterIcon } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { useEditorStore } from "@/store/use-editor-store";
@@ -18,6 +18,32 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import FontFamily from "@tiptap/extension-font-family";
+
+const HighlightColorButton= () => {
+    const { editor } = useEditorStore();
+    const value = editor?.getAttributes("highlight").color || "#ffffff";
+    const onChange = (color: ColorResult)=>{
+        editor?.chain().focus().setHighlight({color: color.hex}).run();
+    };
+    return(
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button
+                className=
+                    "h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+                >
+                    <HighlighterIcon className="size-4"/>
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p-0">
+                <SketchPicker
+                color={value}
+                onChange={onChange}
+                />
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
 
 const TextColorButton= () => {
     const { editor } = useEditorStore();
@@ -254,7 +280,9 @@ export const Toolbar = () => {
                 <ToolbarButton key={item.label} {...item} />
             ))}
             <TextColorButton/>
-            {/*TODO: Highlight Color*/}
+
+            <HighlightColorButton/>
+
             <Separator orientation="vertical" className="h-6 bg-neutral-300" />
             {/*TODO: Link*/}
             {/*TODO: Image*/}
