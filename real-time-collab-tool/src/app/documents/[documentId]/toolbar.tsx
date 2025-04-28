1085f6f1f827ed81d24fff30e8ed256b22d3d198
arshbhushan <arshbhushan@gmail.com>
2025-04-28 15:00:39 +0530

    implemented AlignButton component.

--- real-time-collab-tool/src/app/documents/[documentId]/toolbar.tsx
+++ real-time-collab-tool/src/app/documents/[documentId]/toolbar.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { type ColorResult, SketchPicker } from "react-color";
-import { LucideIcon, Undo2Icon, Redo2Icon, PrinterIcon, SpellCheckIcon, BoldIcon, ItalicIcon, UnderlineIcon, MessageSquareCodeIcon, MessageSquarePlusIcon, ListTodoIcon, RemoveFormattingIcon, ChevronDownIcon, HighlighterIcon, Link2Icon, Upload, UploadIcon, SearchIcon, ImageIcon } from "lucide-react";
+import { LucideIcon, Undo2Icon, Redo2Icon, PrinterIcon, SpellCheckIcon, BoldIcon, ItalicIcon, UnderlineIcon, MessageSquareCodeIcon, MessageSquarePlusIcon, ListTodoIcon, RemoveFormattingIcon, ChevronDownIcon, HighlighterIcon, Link2Icon, Upload, UploadIcon, SearchIcon, ImageIcon, AlignLeftIcon, AlignCenterIcon, AlignRightIcon, AlignJustifyIcon, ListIcon, ListOrderedIcon } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { useEditorStore } from "@/store/use-editor-store";
@@ -29,6 +29,61 @@ import {
     DialogTrigger,
 } from "@/components/ui/dialog"
 import FontFamily from "@tiptap/extension-font-family";
+import TextAlign from "@tiptap/extension-text-align";
+
+const AlignButton = () => {
+    const { editor } = useEditorStore();
+    const alignments = [
+        {
+            label: "Align Left",
+            value: "left",
+            icon: AlignLeftIcon
+        },
+        {
+            label: "Align Center",
+            value: "center",
+            icon: AlignCenterIcon
+        },
+        {
+            label: "Align Right",
+            value: "right",
+            icon: AlignRightIcon
+        },
+        {
+            label: "Align Justify",
+            value: "justify",
+            icon: AlignJustifyIcon
+        },
+
+    ]
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button
+                    className=
+                    "h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+                >
+                    <AlignLeftIcon className="size-4" />
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+                {alignments.map(({label, value, icon: Icon}) =>(
+                    <button
+                    key={value}
+                    onClick={() => editor?.chain().focus().setTextAlign(value).run()}
+                    className={cn(
+                        "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                        editor?.isActive({TextAlign: value})&& "bg-neutral-200/80"
+                    )}
+                    >
+                        <Icon className="size-4"/>
+                        <span className="text-sm">{label}</span>
+                    </button>
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
 
 const ImageButton = () => {
     const { editor } = useEditorStore();
@@ -418,9 +473,9 @@ export const Toolbar = () => {
             <Separator orientation="vertical" className="h-6 bg-neutral-300" />
             <LinkButton />
             <ImageButton/>
-            {/*TODO: Align*/}
+            <AlignButton/>
             {/*TODO: Height*/}
-            {/*TODO: List*/}
+            {/* <ListButton/> */}
             {sections[2].map((item) => (
                 <ToolbarButton key={item.label} {...item} />
             ))}
