0cb4797676f3eb6ba2062d09977336cc18eaec6e
arshbhushan <arshbhushan@gmail.com>
2025-05-02 15:06:13 +0530

    completed line-height custom ext.

--- real-time-collab-tool/src/extensions/line-height.ts
+++ real-time-collab-tool/src/extensions/line-height.ts
@@ -1,4 +1,4 @@
-import { Extension } from "@tiptap/react";
+import { Extension, getAttributes } from "@tiptap/react";
 import "@tiptap/extension-text-style";
 
 declare module "@tiptap/core" {
@@ -9,3 +9,74 @@ declare module "@tiptap/core" {
     };
   }
 }
+
+export const LineHeightExtension = Extension.create({
+    name: "lineHeight",
+    addOptions(){
+        return{
+            types: ["paragraph", "heading"],
+            defaultLineHeight: "normal",
+        }
+    },
+    addGlobalAttributes(){
+        return [
+            {
+                types: this.options.types,
+                attributes: {
+                    lineHeight:{
+                        default: this.options.defaultLineHeight,
+                        renderHTML: attributes=>{
+                            if(!attributes.lineHeight) return{}
+                            return{
+                                style: `line-height:  ${attributes.lineHeight}`,
+                                 
+                            }
+                        },
+                        parseHTML: element=>{
+                            return element.style.lineHeight || this.options.defaultLineHeight
+                        }
+                    }
+                }
+            }
+        ]
+    },
+    addCommands(){
+        return{
+            setLineHeight: (lineHeight: string)=>({tr,state, dispatch})=>{
+                const {selection}= state;
+                tr= tr.setSelection(selection);
+
+                const {from, to}= selection;
+                state.doc.nodesBetween(from, to, (node,pos)=>{
+                    if(this.options.types.includes(node.type.name)){
+                        tr = tr.setNodeMarkup(pos, undefined,{
+                            ...node.attrs,
+                            lineHeight,
+                        })
+                    }
+                })
+
+                if(dispatch) dispatch(tr)
+                    return true;
+            },
+            unsetLineHeight: ()=>({tr, state, dispatch})=>{
+                const {selection} = state;
+                tr = tr.setSelection(selection);
+
+                const {from, to}= selection;
+                state.doc.nodesBetween(from, to , (node,pos)=>{
+                    if(this.options.types.includes(node.type.name)){
+                        tr = tr.setNodeMarkup(pos, undefined,{
+                            ...node.attrs,
+                            lineHeight: this.options.defaultLineHeight  
+                        })
+                    }
+                })
+
+                if(dispatch)dispatch(tr)
+                    return true;
+            }
+        }
+
+    },
+})
