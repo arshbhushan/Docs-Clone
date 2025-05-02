b849c062b3496298a4719879306318aadfc0afda
arshbhushan <arshbhushan@gmail.com>
2025-05-02 14:47:32 +0530

    initated line-height ext.

---
+++ real-time-collab-tool/src/extensions/line-height.ts
@@ -0,0 +1,11 @@
+import { Extension } from "@tiptap/react";
+import "@tiptap/extension-text-style";
+
+declare module "@tiptap/core" {
+  interface Commands<ReturnType> {
+    lineHeight: {
+      setLineHeight: (lineHeight: string) => ReturnType;
+      unsetLineHeight: () => ReturnType;
+    };
+  }
+}
