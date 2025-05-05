6b78929460bf043bf07e0ebdb81635acfca02d0d
arshbhushan <arshbhushan@gmail.com>
2025-05-05 23:45:06 +0530

    imported ruler in editor.

--- real-time-collab-tool/src/app/documents/[documentId]/editor.tsx
+++ real-time-collab-tool/src/app/documents/[documentId]/editor.tsx
@@ -19,6 +19,7 @@ import ImageResize from 'tiptap-extension-resize-image';
 import { useEditorStore } from '@/store/use-editor-store';
 import { LineHeightExtension } from "@/extensions/line-height";
 import { fontSizeExtension } from '@/extensions/font-size';
+import { Ruler } from './ruler';
 
 export const Editor = () => {
     const {setEditor}= useEditorStore();
@@ -106,6 +107,7 @@ export const Editor = () => {
     })
     return (
         <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+            <Ruler/>  
             <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
             <EditorContent editor={editor}/>
             </div>
