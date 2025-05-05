e2d7eab113fea79eafe971bc109526174391d5dc
arshbhushan <arshbhushan@gmail.com>
2025-05-05 23:58:30 +0530

    working on styling and functionality of marker.

--- real-time-collab-tool/src/app/documents/[documentId]/ruler.tsx
+++ real-time-collab-tool/src/app/documents/[documentId]/ruler.tsx
@@ -1,5 +1,31 @@
-export const Ruler = ()=>{
-    return(
-        <div>ruler</div>
+const markers = Array.from({ length: 83 }, (_, i) => i)
+
+export const Ruler = () => {
+    return (
+        <div className="h-6 border-b border-gray-300 flex items-end relative select-none print:hidden">
+            <div
+                id="ruler-container"
+                className="max-w-[816px] mx-auto w-full h-full relative"
+            >
+                <div className="absolute inset-x-0 bottom-0 h-full">
+                    <div className="relative h-full w-[816px]">
+                        {markers.map((marker) => {
+                            const position = (marker * 816) / 82;
+                        return(
+                            <div
+                            key = {marker}
+                            className="absolute bottom-0"
+                            style={{left: `${position}px`}}
+                            >
+                                
+
+                            </div>
+                        )
+                    })}
+                    </div>
+
+                </div>
+            </div>
+            ruler</div>
     );
 };
