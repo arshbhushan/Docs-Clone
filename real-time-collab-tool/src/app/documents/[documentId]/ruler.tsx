9ff6148501738fc9a10211367f141b8d0953eadc
arshbhushan <arshbhushan@gmail.com>
2025-05-08 16:29:52 +0530

    added more marker to fill up space.

--- real-time-collab-tool/src/app/documents/[documentId]/ruler.tsx
+++ real-time-collab-tool/src/app/documents/[documentId]/ruler.tsx
@@ -30,6 +30,11 @@ export const Ruler = () => {
                              {marker % 5 === 0 && marker % 10 !== 0 &&(
                                 <div className="absolute bottom-0 w-[1px] h-1.5 bg-neutral-500"/>
                              )}
+                             {
+                                marker % 5 !==0 &&(
+                                    <div className="absolute bottom-0 w-[1px] h-1 bg-neutral-500"/>  
+                                )
+                             }
 
                             </div>
                         )
