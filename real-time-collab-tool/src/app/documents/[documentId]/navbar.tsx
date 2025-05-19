2a7b45ce0ca9d54d362e3544403daf6e3cad9b69
arshbhushan <arshbhushan@gmail.com>
2025-05-20 03:28:40 +0530

    navbar done.

--- real-time-collab-tool/src/app/documents/[documentId]/navbar.tsx
+++ real-time-collab-tool/src/app/documents/[documentId]/navbar.tsx
@@ -1,5 +1,20 @@
-import Image from "next/image"
-import Link from "next/link"
+import Link from "next/link";
+import Image from "next/image";
+import { FileIcon } from "lucide-react";
+
+import {
+    Menubar,
+    MenubarContent,
+    MenubarItem,
+    MenubarMenu,
+    MenubarSeparator,
+    MenubarShortcut,
+    MenubarSub,
+    MenubarSubContent,
+    MenubarSubTrigger,
+    MenubarTrigger
+} from "@/components/ui/menubar";
+
 import { DocumentInput } from "./document-input"
 
 export const Navbar = () => {
@@ -10,7 +25,23 @@ export const Navbar = () => {
                     <Image src="/logo.svg" alt="Logo" width={36} height={36} />
                 </Link>
                 <div className="flex flex-col">
-                    <DocumentInput />
+                    <div className="flex">
+                        <Menubar className="border-none bg-transparent shadow-none h-auto p-0">
+                            <MenubarMenu>
+                                <MenubarTrigger>
+                                    File
+                                </MenubarTrigger>
+                                <MenubarContent>
+                                    <MenubarItem>
+                                        <FileIcon className="size-4 mr-2"/>
+                                        Save
+                                    </MenubarItem>
+                                </MenubarContent>
+                            </MenubarMenu>
+
+                        </Menubar>
+
+                    </div>
                 </div>
             </div>
         </nav>
