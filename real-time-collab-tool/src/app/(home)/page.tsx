c45493d156297d21ebb955ac578a98cb3e100972
arshbhushan <arshbhushan@gmail.com>
2025-05-23 23:07:44 +0530

    moved page in (home)

--- real-time-collab-tool/src/app/(home)/page.tsx
+++ real-time-collab-tool/src/app/(home)/page.tsx
@@ -1,7 +1,11 @@
-const DocumentsPage = () =>{
-    return( 
-    <div>Documents Page</div>
-);
+ import Link from "next/link";
+
+const Home = () => {
+  return(
+    <div className="flex min-h-screen items-center justify-center">
+      Click<Link href="/documents/123"><span className="text-blue-500 underline">&nbsp;here&nbsp;</span></Link> to go to document IDs. These are currently static.
+    </div>
+  )
 }
 
-export default DocumentsPage;
+export default Home;
