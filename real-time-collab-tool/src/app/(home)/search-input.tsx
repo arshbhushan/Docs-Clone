6242d987c052ce5ef38e9140642433410c6b9b01
arshbhushan <arshbhushan@gmail.com>
2025-05-26 15:33:09 +0530

    added styles in search inputs.

--- real-time-collab-tool/src/app/(home)/search-input.tsx
+++ real-time-collab-tool/src/app/(home)/search-input.tsx
@@ -1,7 +1,16 @@
-export const SearchInput = () =>{
-    return(
-        <div>
-            search input
+import { Input } from "@/components/ui/input";
+
+export const SearchInput = () => {
+
+    return (
+        <div className="flex-1 flex items-center justify-center">
+            <form
+                className="relative max-w-[720px] w-full"
+            >
+                <Input
+                    placeholder="Search"
+                    className="md: text-base Oplaceholder: text-neutral-800 px-14 w-full border-none focus-visible:shadow-[]" />
+            </form>
         </div>
-    )
-}
+    );
+};
