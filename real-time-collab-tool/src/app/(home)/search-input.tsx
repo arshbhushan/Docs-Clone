4747e4401008e8f8b5da12cf00796c2e2dab17f8
arshbhushan <arshbhushan@gmail.com>
2025-05-27 15:58:53 +0530

    working on styling search input.

--- real-time-collab-tool/src/app/(home)/search-input.tsx
+++ real-time-collab-tool/src/app/(home)/search-input.tsx
@@ -9,8 +9,8 @@ export const SearchInput = () => {
             >
                 <Input
                     placeholder="Search"
-                    className="md: text-base Oplaceholder: text-neutral-800 px-14 w-full border-none focus-visible:shadow-[]" />
+                    className="md: text-base Oplaceholder: text-neutral-800 px-14 w-full border-none focus-visible:shadow-[0_1px_1px_0_rgba(65,69,73,.3),0_1px_3px_1px_rgba(65,69,73,.15)]  bg-[#F0F4F8] rounded-full h-[48px] focus-visible:ring-0 focus:bg-white" />
             </form>
         </div>
     );
-};
+}; 
