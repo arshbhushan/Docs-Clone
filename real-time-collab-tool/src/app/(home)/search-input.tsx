a524216a017d8bfaed70d7f6d246b5112b58d1af
arshbhushan <arshbhushan@gmail.com>
2025-05-28 03:36:48 +0530

    added searchIcon in search bar.

--- real-time-collab-tool/src/app/(home)/search-input.tsx
+++ real-time-collab-tool/src/app/(home)/search-input.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { SearchIcon } from "lucide-react";
 
 export const SearchInput = () => {
 
@@ -9,8 +11,19 @@ export const SearchInput = () => {
             >
                 <Input
                     placeholder="Search"
-                    className="md: text-base Oplaceholder: text-neutral-800 px-14 w-full border-none focus-visible:shadow-[0_1px_1px_0_rgba(65,69,73,.3),0_1px_3px_1px_rgba(65,69,73,.15)]  bg-[#F0F4F8] rounded-full h-[48px] focus-visible:ring-0 focus:bg-white" />
+                    className="md: text-base Oplaceholder: text-neutral-800 px-14 w-full border-none 
+                    focus-visible:shadow-[0_1px_1px_0_rgba(65,69,73,.3),0_1px_3px_1px_rgba(65,69,73,.15)]  
+                    bg-[#F0F4F8] rounded-full h-[48px] focus-visible:ring-0 focus:bg-white" 
+                    />
+                    <Button
+                    type="submit"
+                    variant="ghost"
+                    size="icon"
+                    className="absolute left-3 top-1/2 -translate-y-1/2 [&_svg]:size-5 rounded-full"
+                    >
+                        <SearchIcon/>
+                    </Button>
             </form>
         </div>
     );
-}; 
+};
