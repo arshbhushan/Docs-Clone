4e230bbca98446c7a4d4a5e0ac27c4699bc0e51e
arshbhushan <arshbhushan@gmail.com>
2025-05-30 04:49:15 +0530

    final schema to be used for docs.

--- real-time-collab-tool/convex/schema.ts
+++ real-time-collab-tool/convex/schema.ts
@@ -5,5 +5,15 @@ import { title } from "process";
 export default defineSchema({
     documents: defineTable({
         title: v.string(),
-    }),
-  });
+        initialContent: v.optional(v.string()),
+        ownerId: v.string(),
+        roomId: v.optional(v.string()),
+        organizationId: v.optional(v.string()),
+    })
+    .index("by_owner_id", ["ownerId"])
+    .index("by_orginization_id", ["organizationId"])
+    .searchIndex("search_title",{
+        searchField: "title",
+        filterFields: ["ownerId","organizationId"]
+    })
+});
