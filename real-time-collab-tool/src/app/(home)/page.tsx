75b9d4710c629b97e0da2c32a03a97e937add988
arshbhushan <arshbhushan@gmail.com>
2025-05-30 04:48:58 +0530

    added the query logic to show up on the page.

--- real-time-collab-tool/src/app/(home)/page.tsx
+++ real-time-collab-tool/src/app/(home)/page.tsx
@@ -1,16 +1,31 @@
-import Link from "next/link";
+"use client";
+
+import { useQuery } from "convex/react";
 import { Navbar } from "./navbar";
 import { TemplatesGallery } from "./templates-galery";
+import { api } from "../../../convex/_generated/api";
 
 const Home = () => {
-  return(
+
+  const documents = useQuery(api.documents.get);
+
+  if (documents === undefined) {
+    return (
+      <p>Loading...</p>
+    )
+  }
+  return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
-        <Navbar/>
+        <Navbar />
       </div>
 
       <div className="mt-16">
-        <TemplatesGallery/>
+        <TemplatesGallery />
+        {documents?.map((document) => (
+          //added "?" above because: 
+          <span key={document._id}>{document.title}</span>
+        ))}
       </div>
     </div>
   )
