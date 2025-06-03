3a7da5a371ea78b807b61e478a91bfcd1e829db2
arshbhushan <arshbhushan@gmail.com>
2025-06-03 16:40:20 +0530

    convx-client

---
+++ real-time-collab-tool/src/components/convex-client-provider.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { ReactNode } from "react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ClerkProvider, SignIn, useAuth } from "@clerk/nextjs";
+import { ConvexReactClient, Authenticated, Unauthenticated, AuthLoading } from "convex/react";
+import { FullscreenLoader } from "./fullscreen-loader";
+
+const convex = new ConvexReactClient("URL");
+
+
+export function ConvexClientProvider({ children }: { children: ReactNode }) {
+    return (
+        <ClerkProvider publishableKey="Key">
+            <ConvexProviderWithClerk
+                useAuth={useAuth}
+                client={convex}
+            >
+                <Authenticated>
+                {children}
+                </Authenticated>
+                <Unauthenticated>
+                    <div className="flex flex-col items-center justify-center min-h-screen">
+                        <SignIn routing="hash"/>
+                    </div>
+                    <p>Please login</p>
+                </Unauthenticated>
+                <AuthLoading>
+                    <FullscreenLoader label="Auth Loading..."/>
+                </AuthLoading>
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
+    )
+
+};
