7d676427870e031d749fff1ac3da7fadc7603a8d
arshbhushan <arshbhushan@gmail.com>
2025-06-05 05:22:23 +0530

    added document row for presenting the docs in a proper manner.

---
+++ real-time-collab-tool/src/app/(home)/document-row.tsx
@@ -0,0 +1,14 @@
+import { TableCell, TableRow } from "@/components/ui/table"
+import { Doc } from "../../../convex/_generated/dataModel";
+
+interface DocumentRowProps{
+    document: Doc<"documents">;
+}
+
+export const DocumentRow = ({document}: DocumentRowProps)=>{
+    return (
+        <TableRow>
+            
+        </TableRow>
+    )
+}
