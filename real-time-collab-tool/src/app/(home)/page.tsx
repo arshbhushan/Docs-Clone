c88ed6341306b960b1b937b5894a281c0a4b2971
arshbhushan <arshbhushan@gmail.com>
2025-06-05 05:45:36 +0530

    integrated usePaginatedQuery and DocumentTable.

--- real-time-collab-tool/src/app/(home)/page.tsx
+++ real-time-collab-tool/src/app/(home)/page.tsx
@@ -1,19 +1,19 @@
 "use client";
 
-import { useQuery } from "convex/react";
+import { usePaginatedQuery } from "convex/react";
 import { Navbar } from "./navbar";
+import { DocumentTable } from "./documents-table";
 import { TemplatesGallery } from "./templates-galery";
 import { api } from "../../../convex/_generated/api";
 
 const Home = () => {
 
-  const documents = useQuery(api.documents.get);
+  const {
+    results,
+    status, 
+    loadMore
+  } = usePaginatedQuery(api.documents.get, {}, {initialNumItems: 5}); //{} here is for any other argument we will have later. 
 
-  if (documents === undefined) {
-    return (
-      <p>Loading...</p>
-    )
-  }
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -22,10 +22,11 @@ const Home = () => {
 
       <div className="mt-16">
         <TemplatesGallery />
-        {documents?.map((document) => (
-          //added "?" above because: 
-          <span key={document._id}>{document.title}</span>
-        ))}
+        <DocumentTable
+        documents= {results}
+        loadMore = {loadMore}
+        status= {status}
+        />
       </div>
     </div>
   )
