f79bb0c6523fde4614b1bd21d7c6bf69993617e1
arshbhushan <arshbhushan@gmail.com>
2025-06-07 04:08:07 +0530

    integrated open in new tab functionality.

--- real-time-collab-tool/src/app/(home)/document-menu.tsx
+++ real-time-collab-tool/src/app/(home)/document-menu.tsx
@@ -1,11 +1,38 @@
-import { Button } from "@/components/ui/button"
-import { MoreVertical } from "lucide-react"
+import { ExternalLinkIcon, MoreVertical } from "lucide-react";
+import { Id } from "../../../convex/_generated/dataModel";
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
-export const DocumentMenu = () => {
+interface DocumentMenuProps {
+    documentId: Id<"documents">;
+    title: string;
+    onNewTab: (id: Id<"documents">) => void;
+};
+
+export const DocumentMenu = ({ documentId, title, onNewTab }: DocumentMenuProps) => {
 
     return (
-        <Button variant="ghost" size="icon" className="rounded-full">
-            <MoreVertical className="size-4" />
-        </Button>
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" size="icon" className="rounded-full">
+                    <MoreVertical className="size-4" />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem
+                    onClick={()=>onNewTab(documentId)}
+                >
+                    <ExternalLinkIcon className="size-4 mr-2"/> 
+                    Open in a new tab
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
     )
 }
