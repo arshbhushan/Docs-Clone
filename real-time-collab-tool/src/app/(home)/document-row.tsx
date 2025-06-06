aba4876b165ff803d43160f38174add4b6af7816
arshbhushan <arshbhushan@gmail.com>
2025-06-07 04:05:45 +0530

    added open in new tab functionality.

--- real-time-collab-tool/src/app/(home)/document-row.tsx
+++ real-time-collab-tool/src/app/(home)/document-row.tsx
@@ -6,12 +6,16 @@ import { TableCell, TableRow } from "@/components/ui/table";
 
 import { Doc } from "../../../convex/_generated/dataModel";
 import { Button } from "@/components/ui/button";
+import { DocumentMenu } from "./document-menu";
 
 interface DocumentRowProps {
     document: Doc<"documents">;
 }
 
 export const DocumentRow = ({ document }: DocumentRowProps) => {
+    const onNewTabClick = (id:string)=>{
+        window.open(`/documents/${id}`, "_blank");
+    }
     return (
         <TableRow
             className="cursor-pointer"
@@ -33,9 +37,11 @@ export const DocumentRow = ({ document }: DocumentRowProps) => {
                 {format(new Date(document._creationTime), "MMM dd, yyyy")}
             </TableCell>
             <TableCell className="flex ml-auto justify-end">
-                <Button variant="ghost" size="icon" className="rounded-full">
-                    <MoreVertical className="size-4"/> 
-                </Button>
+                <DocumentMenu
+                    documentId = {document._id}
+                    title ={document.title}
+                    onNewTab={onNewTabClick}
+                />
             </TableCell>
 
         </TableRow>
