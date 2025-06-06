91978b4b9f22d5708f7916520f8a1630d508ef6d
arshbhushan <arshbhushan@gmail.com>
2025-06-07 04:15:49 +0530

    added delete functionality.

--- real-time-collab-tool/convex/documents.ts
+++ real-time-collab-tool/convex/documents.ts
@@ -1,17 +1,17 @@
-import {ConvexError, v} from "convex/values"
+import { ConvexError, v } from "convex/values"
 import { paginationOptsValidator } from "convex/server";
 import { mutation, query } from "./_generated/server";
 
 export const create = mutation({
-    args: {title: v.optional(v.string()), initialContent: v.optional(v.string())},
-    handler: async ( ctx, args) =>{
+    args: { title: v.optional(v.string()), initialContent: v.optional(v.string()) },
+    handler: async (ctx, args) => {
         const user = await ctx.auth.getUserIdentity();
 
-        if(!user){
+        if (!user) {
             throw new ConvexError("Unauthorized ")
         }
 
-         return await ctx.db.insert("documents",{
+        return await ctx.db.insert("documents", {
             title: args.title ?? "untitled document",
             ownerId: user.subject,
             initialContent: args.initialContent,
@@ -21,8 +21,35 @@ export const create = mutation({
 })
 
 export const get = query({
-    args: {paginationOpts: paginationOptsValidator},
-  handler: async (ctx,args) => {
-    return await ctx.db.query("documents").paginate(args.paginationOpts);
-  },
+    args: { paginationOpts: paginationOptsValidator },
+    handler: async (ctx, args) => {
+        return await ctx.db.query("documents").paginate(args.paginationOpts);
+    },
 });
+
+export const removeById = mutation({
+    args: { id: v.id("documents") },
+    handler: async (ctx, args) => {
+        const user = await ctx.auth.getUserIdentity();
+
+        if (!user) {
+            throw new ConvexError("Unauthorized");
+        }
+
+        const document = await ctx.db.get(args.id);
+
+        if (!document) {
+            throw new ConvexError("Document not found");
+        }
+
+        const isOwner = document.ownerId === user.subject;
+
+        if (!isOwner) {
+            throw new ConvexError("Unauthorized");
+        }
+
+        return await ctx.db.delete(args.id);
+    },
+})
+
+//6:25:49
