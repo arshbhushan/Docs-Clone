da79b2e3eb60b5a51e1ddff0c1b4dcb8d55dfd28
arshbhushan <arshbhushan@gmail.com>
2025-06-11 04:28:43 +0530

    integrated the remove-dialog changes for the three dots.

--- real-time-collab-tool/src/app/(home)/document-menu.tsx
+++ real-time-collab-tool/src/app/(home)/document-menu.tsx
@@ -1,4 +1,4 @@
-import { ExternalLinkIcon, MoreVertical } from "lucide-react";
+import { ExternalLinkIcon, MoreVertical, TrashIcon } from "lucide-react";
 import { Id } from "../../../convex/_generated/dataModel";
 import { Button } from "@/components/ui/button";
 import {
@@ -9,6 +9,7 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { RemoveDialog } from "@/components/remove-dialog";
 
 interface DocumentMenuProps {
     documentId: Id<"documents">;
@@ -26,6 +27,15 @@ export const DocumentMenu = ({ documentId, title, onNewTab }: DocumentMenuProps)
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent>
+                <RemoveDialog documentId={documentId}>
+                    <DropdownMenuItem
+                    onSelect={(e)=>e.preventDefault()}
+                    onClick={(e)=>e.stopPropagation()}
+                    >
+                        <TrashIcon className="size-4 mr-2"/>
+                        Remove
+                    </DropdownMenuItem>
+                </RemoveDialog>
                 <DropdownMenuItem
                     onClick={()=>onNewTab(documentId)}
                 >
