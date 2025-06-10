525c480fd3005f5cedc82a78e8f4a1ca9e546413
arshbhushan <arshbhushan@gmail.com>
2025-06-11 04:28:04 +0530

    added content for the UI.

--- real-time-collab-tool/src/components/remove-dialog.tsx
+++ real-time-collab-tool/src/components/remove-dialog.tsx
@@ -9,23 +9,38 @@ import {
     AlertDialogHeader,
     AlertDialogTitle,
     AlertDialogTrigger,
-  } from "@/components/ui/alert-dialog";
+} from "@/components/ui/alert-dialog";
 
-  import { Id } from "../../convex/_generated/dataModel";
+import { Id } from "../../convex/_generated/dataModel";
 
 
-  interface RemoveDialogProps{
+interface RemoveDialogProps {
     documentId: Id<"documents">;
     children: React.ReactNode
-  };
+};
 
-  export const RemoveDialog = ({documentId, children}: RemoveDialogProps)=>{
-    return(
+export const RemoveDialog = ({ documentId, children }: RemoveDialogProps) => {
+    return (
         <AlertDialog>
             <AlertDialogTrigger asChild>
                 {children}
-
             </AlertDialogTrigger>
+            <AlertDialogContent onClick={(e) => e.stopPropagation()}>
+                <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                    This action cannot be reverted. This will be premanently delete this document.
+                </AlertDialogDescription>
+                <AlertDialogHeader>
+                    <AlertDialogFooter>
+                        <AlertDialogCancel onClick={(e) => e.stopPropagation()}>
+                            Cancel
+                        </AlertDialogCancel>
+                        <AlertDialogAction>
+                            Delete
+                        </AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogHeader>
+            </AlertDialogContent>
         </AlertDialog>
     )
-  }
+}
