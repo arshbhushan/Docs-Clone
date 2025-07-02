211c2219c133445ffb5e6217d3f94aba66ffbcff
arshbhushan <arshbhushan@gmail.com>
2025-07-03 04:31:25 +0530

    added a load more option if the docs are >5.

--- real-time-collab-tool/src/app/(home)/documents-table.tsx
+++ real-time-collab-tool/src/app/(home)/documents-table.tsx
@@ -13,6 +13,7 @@ import {
 import { Doc } from "../../../convex/_generated/dataModel";
 import { LoaderIcon } from "lucide-react";
 import { DocumentRow } from "./document-row";
+import { Button } from "@/components/ui/button";
 
 interface DocumentsTableProps {
     documents: Doc<"documents">[] | undefined;
@@ -60,6 +61,17 @@ export const DocumentTable = ({
                     )}
                 </Table>
             )}
+            <div className="flex items-center justify-center">
+                <Button 
+                variant="ghost" 
+                size="sm" 
+                onClick={()=>loadMore(5)} 
+                disabled={status !== "CanLoadMore"}
+                >
+                    {status === "CanLoadMore" ? "Load more": "End of results"} 
+                </Button>
+
+            </div>
         </div>
     )
 }
