31f98a7b6a5dd81376b4c70474062c197e088dc0
arshbhushan <arshbhushan@gmail.com>
2025-07-05 04:32:06 +0530

    Implemented the comment functionality.

--- real-time-collab-tool/src/app/documents/[documentId]/toolbar.tsx
+++ real-time-collab-tool/src/app/documents/[documentId]/toolbar.tsx
@@ -606,8 +606,8 @@ export const Toolbar = () => {
                 {
                     label: "Comment",
                     icon: MessageSquarePlusIcon,
-                    onClick: () => console.log("TODO: Comment"),
-                    isActive: false,
+                    onClick: () => editor?.chain().focus().addPendingComment().run(),
+                    isActive: editor?.isActive("LiveblocksCommentMark"),
                 },
                 {
                     label: "List",
