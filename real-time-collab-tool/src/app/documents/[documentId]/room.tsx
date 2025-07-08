4a0b9925755e7120782e19ea79f2cb93240d90c2
arshbhushan <arshbhushan@gmail.com>
2025-07-08 15:27:41 +0530

    using private key instead of public key.

--- real-time-collab-tool/src/app/documents/[documentId]/room.tsx
+++ real-time-collab-tool/src/app/documents/[documentId]/room.tsx
@@ -11,7 +11,10 @@ import { useParams } from "next/navigation";
 export function Room({ children }: { children: ReactNode }) {
     const params = useParams();
   return (
-    <LiveblocksProvider publicApiKey={"pk_dev_fZBWrfiNeuy9TT5AvOGPdpabgFb04CofwOzIWFuo9WttlDOPZG2Te3Y3OJh1f2Ns"}>
+    <LiveblocksProvider 
+      throttle={16}
+      authEndpoint="/api/liveblocks-auth"
+    >
       <RoomProvider id={params.documentId as string}>
         <ClientSideSuspense fallback={<div>Loading…</div>}>
           {children}
